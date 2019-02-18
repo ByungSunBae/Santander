802a12e2ff15e9026158409ca26558b192dcb1b4
ByungSunBae <byung2070@gmail.com>
2019-02-18 13:45:52 +0900

    parallel processing in for loop

--- feature_engineering.R
+++ feature_engineering.R
@@ -1,12 +1,16 @@
 library("tidyverse")
+library("data.table")
+library("dtplyr")
 library("Information")
 library("moments")
 
-fread <- data.table::fread
-fwrite <- data.table::fwrite
+# install.packages(c("Information", "moments"))
 
-train_df <- fread("data/train.csv", data.table = FALSE) %>% as_tibble()
-test_df <- fread("data/test.csv", data.table = FALSE) %>% as_tibble()
+# fread <- data.table::fread
+# fwrite <- data.table::fwrite
+
+train_df <- fread("data/train.csv", data.table = TRUE)
+test_df <- fread("data/test.csv", data.table = TRUE)
 
 all_vars <- names(train_df)
 target <- "target"
@@ -67,7 +71,7 @@ crv <- 0.05
 
 history_v <- c()
 
-for(i in 1:60){
+for(i in 1:100){
   cat(i, "th----\n")
   smpd_vars <- sort(sample(features, 
                            size = sample(2:120, size = 1), 
@@ -77,88 +81,84 @@ for(i in 1:60){
   cat("# of Sample variables:", length(smpd_vars), "\n")
   
   if(sum(history_v %in% paste(smpd_vars, collapse = ".")) >= 1){
-    next
+    cat("None..\n")
   } else {
     if(length(smpd_vars) < 30){
-      Add_variables <- train_df %>% 
-        select_at(c(smpd_vars, target)) %>% 
-        transmute(
-          sqmean = apply(select(., smpd_vars), 1, function(x) mean(x^2)),
-          soft_max = apply(select(., smpd_vars), 1, function(x) max(exp(x * 0.1)/sum(exp(x * 0.1)))),
-          exp_mean = apply(select(., smpd_vars), 1, function(x) mean(exp(x))),
-          max = apply(select(., smpd_vars), 1, function(x) max(x)),
-          min = apply(select(., smpd_vars), 1, function(x) min(x)),
-          log_maxabs = apply(select(., smpd_vars), 1, function(x) log(0.5 + max(abs(x)))),
-          log_absmax = apply(select(., smpd_vars), 1, function(x) log(0.5 + abs(max(x)))),
-          max_inv_exp = apply(select(., smpd_vars), 1, function(x) max(1/exp(x))),
-          target = apply(select(., target), 1, function(x) x)
-        )
       
-      Add_variables_test <- test_df %>% 
-        select_at(c(smpd_vars)) %>% 
-        transmute(
-          sqmean = apply(select(., smpd_vars), 1, function(x) mean(x^2)),
-          soft_max = apply(select(., smpd_vars), 1, function(x) max(exp(x * 0.1)/sum(exp(x * 0.1)))),
-          exp_mean = apply(select(., smpd_vars), 1, function(x) mean(exp(x))),
-          max = apply(select(., smpd_vars), 1, function(x) max(x)),
-          min = apply(select(., smpd_vars), 1, function(x) min(x)),
-          log_maxabs = apply(select(., smpd_vars), 1, function(x) log(0.5 + max(abs(x)))),
-          log_absmax = apply(select(., smpd_vars), 1, function(x) log(0.5 + abs(max(x)))),
-          max_inv_exp = apply(select(., smpd_vars), 1, function(x) max(1/exp(x)))
-        )
+      mk_vars_train <- c("ID_code", "sqmean", "soft_max", "exp_mean", "max", "min", "log_maxabs", "log_absmax", "max_inv_exp", "target")
+      mk_vars_test <- c("ID_code", "sqmean", "soft_max", "exp_mean", "max", "min", "log_maxabs", "log_absmax", "max_inv_exp")
       
-      IV_table_t <- create_infotables(data = Add_variables, y = target)
-      IV_sum <- IV_table_t$Summary
     } else {
-      Add_variables <- train_df %>% 
-        select_at(c(smpd_vars, target)) %>% 
-        transmute(
-          sqmean = apply(select(., smpd_vars), 1, function(x) mean(x^2)),
-          soft_max = apply(select(., smpd_vars), 1, function(x) max(exp(x * 0.1)/sum(exp(x * 0.1)))),
-          exp_mean = apply(select(., smpd_vars), 1, function(x) mean(exp(x))),
-          max = apply(select(., smpd_vars), 1, function(x) max(x)),
-          min = apply(select(., smpd_vars), 1, function(x) min(x)),
-          log_maxabs = apply(select(., smpd_vars), 1, function(x) log(0.5 + max(abs(x)))),
-          log_absmax = apply(select(., smpd_vars), 1, function(x) log(0.5 + abs(max(x)))),
-          max_inv_exp = apply(select(., smpd_vars), 1, function(x) max(1/exp(x))),
-          sd = apply(select(., smpd_vars), 1, function(x) sd(x)),
-          sd_sq = apply(select(., smpd_vars), 1, function(x) sd(x^2)),
-          skew = apply(select(., smpd_vars), 1, function(x) skewness(x)),
-          skew_sq = apply(select(., smpd_vars), 1, function(x) skewness(x^2)),
-          kurt = apply(select(., smpd_vars), 1, function(x) kurtosis(x)),
-          kurt_sq = apply(select(., smpd_vars), 1, function(x) kurtosis(x^2)),
-          q1_exp = apply(select(., smpd_vars), 1, function(x) quantile(exp(x), probs = 0.25)),
-          q3_exp = apply(select(., smpd_vars), 1, function(x) quantile(exp(x), probs = 0.75)),
-          CV_c = apply(select(., smpd_vars), 1, function(x) sd(x)/mean(x)),
-          target = apply(select(., target), 1, function(x) x)
-        )
-      
-      Add_variables_test <- 
-        test_df %>% 
-        select_at(c(smpd_vars)) %>% 
-        transmute(
-          sqmean = apply(select(., smpd_vars), 1, function(x) mean(x^2)),
-          soft_max = apply(select(., smpd_vars), 1, function(x) max(exp(x * 0.1)/sum(exp(x * 0.1)))),
-          exp_mean = apply(select(., smpd_vars), 1, function(x) mean(exp(x))),
-          max = apply(select(., smpd_vars), 1, function(x) max(x)),
-          min = apply(select(., smpd_vars), 1, function(x) min(x)),
-          log_maxabs = apply(select(., smpd_vars), 1, function(x) log(0.5 + max(abs(x)))),
-          log_absmax = apply(select(., smpd_vars), 1, function(x) log(0.5 + abs(max(x)))),
-          max_inv_exp = apply(select(., smpd_vars), 1, function(x) max(1/exp(x))),
-          sd = apply(select(., smpd_vars), 1, function(x) sd(x)),
-          sd_sq = apply(select(., smpd_vars), 1, function(x) sd(x^2)),
-          skew = apply(select(., smpd_vars), 1, function(x) skewness(x)),
-          skew_sq = apply(select(., smpd_vars), 1, function(x) skewness(x^2)),
-          kurt = apply(select(., smpd_vars), 1, function(x) kurtosis(x)),
-          kurt_sq = apply(select(., smpd_vars), 1, function(x) kurtosis(x^2)),
-          q1_exp = apply(select(., smpd_vars), 1, function(x) quantile(exp(x), probs = 0.25)),
-          q3_exp = apply(select(., smpd_vars), 1, function(x) quantile(exp(x), probs = 0.75)),
-          CV_c = apply(select(., smpd_vars), 1, function(x) sd(x)/mean(x))
-        )
-      
-      IV_table_t <- create_infotables(data = Add_variables, y = target)
-      IV_sum <- IV_table_t$Summary
+      mk_vars_train <- c("ID_code", "sqmean", "soft_max", "exp_mean", "max", "min", "log_maxabs", "log_absmax", "max_inv_exp", 
+                   "sd", "sd_sq", "kurt", "kurt_sq", "q1_exp", "q3_exp", "CV_c", "target")
+      mk_vars_test <- c("ID_code", "sqmean", "soft_max", "exp_mean", "max", "min", "log_maxabs", "log_absmax", "max_inv_exp", 
+                   "sd", "sd_sq", "kurt", "kurt_sq", "q1_exp", "q3_exp", "CV_c")
     }
+    
+    selected_vars_train <- c("ID_code", smpd_vars, target)
+    selected_vars_test <- c("ID_code", smpd_vars)
+    
+    Add_variables <- train_df[, selected_vars_train, with = FALSE] %>% 
+      .[, parallel::mclapply(
+        mk_vars_train,
+        function(x){
+          switch(x,
+                 "ID_code" = apply(select(., "ID_code"), 1, function(x) x),
+                 "sqmean" = apply(select(., smpd_vars), 1, function(x) mean(x^2)),
+                 "soft_max" = apply(select(., smpd_vars), 1, function(x) max(exp(x * 0.1)/sum(exp(x * 0.1)))),
+                 "exp_mean" = apply(select(., smpd_vars), 1, function(x) mean(exp(x))),
+                 "max" = apply(select(., smpd_vars), 1, function(x) max(x)),
+                 "min" = apply(select(., smpd_vars), 1, function(x) min(x)),
+                 "log_maxabs" = apply(select(., smpd_vars), 1, function(x) log(0.5 + max(abs(x)))),
+                 "log_absmax" = apply(select(., smpd_vars), 1, function(x) log(0.5 + abs(max(x)))),
+                 "max_inv_exp" = apply(select(., smpd_vars), 1, function(x) max(1/exp(x))),
+                 "sd" = apply(select(., smpd_vars), 1, function(x) sd(x)),
+                 "sd_sq" = apply(select(., smpd_vars), 1, function(x) sd(x^2)),
+                 "skew" = apply(select(., smpd_vars), 1, function(x) skewness(x)),
+                 "skew_sq" = apply(select(., smpd_vars), 1, function(x) skewness(x^2)),
+                 "kurt" = apply(select(., smpd_vars), 1, function(x) kurtosis(x)),
+                 "kurt_sq" = apply(select(., smpd_vars), 1, function(x) kurtosis(x^2)),
+                 "q1_exp" = apply(select(., smpd_vars), 1, function(x) quantile(exp(x), probs = 0.25)),
+                 "q3_exp" = apply(select(., smpd_vars), 1, function(x) quantile(exp(x), probs = 0.75)),
+                 "CV_c" = apply(select(., smpd_vars), 1, function(x) sd(x)/mean(x)),
+                 "target" = apply(select(., target), 1, function(x) x)
+          )
+        }, mc.cores = 6
+      )]
+    
+    names(Add_variables) <- mk_vars_train
+    
+    Add_variables_test <- test_df[, selected_vars_test, with = FALSE] %>% 
+      .[, parallel::mclapply(
+        mk_vars_test,
+        function(x){
+          switch(x,
+                 "ID_code" = apply(select(., "ID_code"), 1, function(x) x),
+                 "sqmean" = apply(select(., smpd_vars), 1, function(x) mean(x^2)),
+                 "soft_max" = apply(select(., smpd_vars), 1, function(x) max(exp(x * 0.1)/sum(exp(x * 0.1)))),
+                 "exp_mean" = apply(select(., smpd_vars), 1, function(x) mean(exp(x))),
+                 "max" = apply(select(., smpd_vars), 1, function(x) max(x)),
+                 "min" = apply(select(., smpd_vars), 1, function(x) min(x)),
+                 "log_maxabs" = apply(select(., smpd_vars), 1, function(x) log(0.5 + max(abs(x)))),
+                 "log_absmax" = apply(select(., smpd_vars), 1, function(x) log(0.5 + abs(max(x)))),
+                 "max_inv_exp" = apply(select(., smpd_vars), 1, function(x) max(1/exp(x))),
+                 "sd" = apply(select(., smpd_vars), 1, function(x) sd(x)),
+                 "sd_sq" = apply(select(., smpd_vars), 1, function(x) sd(x^2)),
+                 "skew" = apply(select(., smpd_vars), 1, function(x) skewness(x)),
+                 "skew_sq" = apply(select(., smpd_vars), 1, function(x) skewness(x^2)),
+                 "kurt" = apply(select(., smpd_vars), 1, function(x) kurtosis(x)),
+                 "kurt_sq" = apply(select(., smpd_vars), 1, function(x) kurtosis(x^2)),
+                 "q1_exp" = apply(select(., smpd_vars), 1, function(x) quantile(exp(x), probs = 0.25)),
+                 "q3_exp" = apply(select(., smpd_vars), 1, function(x) quantile(exp(x), probs = 0.75)),
+                 "CV_c" = apply(select(., smpd_vars), 1, function(x) sd(x)/mean(x)),
+                 "target" = apply(select(., target), 1, function(x) x)
+          )
+        }, mc.cores = 6
+      )]
+    names(Add_variables_test) <- mk_vars_test
+    
+    IV_table_t <- create_infotables(data = Add_variables[, -"ID_code"], y = target)
+    IV_sum <- IV_table_t$Summary
     
     addss <- IV_sum %>% filter(IV >= crv)
     
@@ -167,14 +167,13 @@ for(i in 1:60){
       tmp <- map(addss$Variable, function(x) x) %>% unlist()
       names(tmp) <- paste0("newvar", i, "_", addss$Variable)
       
-      head(train_df)
-      train_df <- 
-        train_df %>% 
-        cbind(., Add_variables %>% select(!!tmp))
+      train_df <-
+        train_df %>%
+        cbind(., Add_variables %>% as_tibble() %>% select(!!tmp))
       
-      test_df <- 
-        test_df %>% 
-        cbind(., Add_variables_test %>% select(!!tmp))
+      test_df <-
+        test_df %>%
+        cbind(., Add_variables_test %>% as_tibble() %>% select(!!tmp))
     } else {
       cat("Hmm.. we can't make variable...\n")
     }
