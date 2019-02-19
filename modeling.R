3a5f17c29d500caa591ef878adfd525a5a538796
ByungSunBae <byung2070@gmail.com>
2019-02-19 13:19:22 +0900

    modeling

--- modeling.R
+++ modeling.R
@@ -1,4 +1,5 @@
 library("data.table")
+library("Information")
 library("h2o")
 
 ths <- parallel::detectCores() - 2
@@ -15,7 +16,15 @@ target <- "target"
 all_vars <- names(train_h2o)
 features <- all_vars[! all_vars %in% c("ID_code", target)]
 
-aml <- h2o.automl(x = features, y = target, training_frame = train_h2o, max_runtime_secs = 8 * 3600)
+aml <-
+  h2o.automl(
+    x = features,
+    y = target,
+    training_frame = train_h2o,
+    max_runtime_secs = 3 * 3600,
+    exclude_algos = c("DRF")
+  )
+
 h2o.saveModel(aml@leader, "./model", force = TRUE)
 
 pred <- as.data.table(h2o.predict(aml@leader, newdata = test_h2o))
@@ -27,4 +36,4 @@ sub_dt <- merge(sub_dt, pred[, c("ID_code", "p1")], by = "ID_code")
 sub_dt[, target := NULL]
 setnames(sub_dt, "p1", "target")
 
-fwrite(sub_dt, "result/submission_v7.csv")
+fwrite(sub_dt, "result/submission_v9.csv")
